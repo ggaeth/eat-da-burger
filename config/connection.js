a7d4027b12a732dd28ae94aa2be535d973638061
ggaeth <34075514+ggaeth@users.noreply.github.com>
2018-04-24 15:49:55 -0500

    tests

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Pull in required dependencies
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	})
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple-Stacked Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Kick Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('West Coast Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Swiss Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hickory BBQ Burger', true);
