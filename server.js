55b357bf3579469087ad3c77e41fed4230a7d925
ggaeth <34075514+ggaeth@users.noreply.github.com>
2018-04-25 15:41:58 -0500

    big push

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-// Pull in required dependencies
+// dependencies for import
 var express = require('express');
 var bodyParser = require('body-parser');
-//var methodOverride = require('method-override');
+var methodOverride = require('method-override');
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the 'public' directory
+// static content will be stored in public folder
 app.use(express.static(process.cwd() + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Override with POST having ?_method=DELETE
-//app.use(methodOverride('_method'));
+// Override for POST
+app.use(methodOverride('_method'));
 
 // Set Handlebars as the view engine
 var exphbs = require('express-handlebars');
@@ -21,7 +21,7 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-// Import routes and give the server access to them
+// import and give server access to routes
 var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
