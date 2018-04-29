beeeccd1dd6890663c013215ed66387ae6fe7e9a
ggaeth <34075514+ggaeth@users.noreply.github.com>
2018-04-29 17:10:49 -0500

    pls

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -26,6 +26,6 @@ var routes = require('./controllers/burgers_controller.js');
 
 app.use('/', routes);
 
-app.listen(port, function() {
-  console.log("App listening on PORT " + port);
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
 });
