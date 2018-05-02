fd7183f3b65550645c643b4958bccfbb23498e1c
ggaeth <34075514+ggaeth@users.noreply.github.com>
2018-05-01 20:34:30 -0500

    pls work

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,11 @@
--- Create the burgers_db database --
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
--- Create a burgers table with the required fields --
+USE tfx19baacr3mp2cx;
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
   	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   	dt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    createdAt TIMESTAMP NOT NULL,
   	PRIMARY KEY(id)
 );
